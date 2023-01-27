a3555a4e54e4432096739f791a457e7f2b834a8c
Wilson <yiqun2529092370@outlook.com>
2023-01-27 19:21:27 +0800

    完善TextPro超链接组件

---
+++ Html2UnityRich/Assets/Html2UnityRich/Component/LinkOpener.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent (typeof (TextMeshProUGUI))]
+public class LinkOpener : MonoBehaviour, IPointerClickHandler
+{
+    public void OnPointerClick (PointerEventData eventData)
+    {
+        TextMeshProUGUI pTextMeshPro = GetComponent<TextMeshProUGUI> ();
+        int linkIndex =
+            TMP_TextUtilities.FindIntersectingLink (pTextMeshPro , eventData.position ,
+                null); // If you are not in a Canvas using Screen Overlay, put your camera instead of null
+        if ( linkIndex != -1 )
+        {
+            // was a link clicked?
+            TMP_LinkInfo linkInfo = pTextMeshPro.textInfo.linkInfo [linkIndex];
+            Application.OpenURL (linkInfo.GetLinkID ());
+        }
+    }
+}
